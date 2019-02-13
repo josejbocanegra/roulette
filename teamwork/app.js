74eecb97c3a28382500125f8cbda271c4d0612d9
Jose Bocanegra <josebocanegra@MacBook-Pro-de-Jose.local>
2019-02-12 21:27:48 -0500

    iteration

--- teamwork/app.js
+++ teamwork/app.js
@@ -1,18 +1,31 @@
 const teamWorkSession = require('node-teamwork');
 const teamWork = new teamWorkSession({ url: 'https://uniandesedu.teamwork.com', token: 'twp_scYsPM0DU3iCKgwrC1WJdREc3wkL'});
-/*
+
 teamWork.listProjects().then(response=>{
     var id=[];
     for (let rsp in response){
         var projects = response[rsp].projects;
         for(let prj in projects){
-            console.log(`Name: ${projects[prj].name}; ID: ${projects[prj].id}`);    
+
+            let id = projects[prj].id; 
+            let name = projects[prj].name;
+            teamWork.getProjectPeople({projectId:id}).then(response=>{
+                console.log("Proyecto name: " + name);
+                for (let people in response.payload){
+                    let person = response.payload[people];
+                    for (let per of person){
+                        console.log(per.id);
+                        console.log(per['user-name']);
+                    }
+                }
+            });
+            //console.log(`Name: ${projects[prj].name}; ID: ${projects[prj].id}`);    
         } 
         
 }});
-*/
 
-teamWork.getProjectTasks({projectId : '494242'}).then(response=>{
+
+/*teamWork.getProjectTasks({projectId : '494242'}).then(response=>{
     for (let tasks in response){
         var task = response[tasks]['todo-items'];
         for(let tk in task){
@@ -21,7 +34,7 @@ teamWork.getProjectTasks({projectId : '494242'}).then(response=>{
             console.log(task[tk].timeIsLogged);  
             console.log(task[tk]['responsible-party-names']);
         }
-}});
+}});*/
 
 
 
