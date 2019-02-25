dc48c03563f25a91d50e83947ab4ec378f8a0e30
Jose Bocanegra <josejbocanegra@gmail.com>
2019-02-25 18:57:03 -0500

    new folder

--- express/cbasicos/main.js
+++ express/cbasicos/main.js
@@ -1,22 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get("/", (req,res)=>{
-	res.end("Raiz");
-});
-
-app.get("/estudiantes", (req,res)=>{
-	res.end("Estudiantes");
-});
-
-app.get("/aulas", (req,res)=>{
-	res.end("Aulas");
-});
-
-app.get("*", (req,res)=>{
-	res.end("Not found");
-});
-
 app.listen(8000);
+
 
 
--- express/cbasicos/previous.js
+++ express/cbasicos/previous.js
@@ -3,6 +3,7 @@ const http = require ("http");
 http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
+
     res.write("<h1>Hola</h1>");
     res.end();
 }).listen(8080);
