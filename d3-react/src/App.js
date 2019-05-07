c314cf146f2a67ff93d37d5c83ce8fb9fbc2ada7
Jose Bocanegra <josejbocanegra@gmail.com>
2019-05-06 19:51:44 -0500

    Adding d3 and react project

--- d3-react/src/App.js
+++ d3-react/src/App.js
@@ -1,47 +1,8 @@
 import React, { Component } from 'react';
 import * as d3 from "d3";
-/*
-const data = [
-	{
-		"country": "Afghanistan",
-		"population": "31889923",
-		"lifeexpectancy": "43.8",
-		"purchasingpower": "974.58"
-	},
-	{
-		"country": "Albania",
-		"population": "3600523",
-		"lifeexpectancy": "76.4",
-		"purchasingpower": "5937.03"
-	},
-	{
-		"country": "Algeria",
-		"population": "33333216",
-		"lifeexpectancy": "72.3",
-		"purchasingpower": "6223.37"
-	},
-	{
-		"country": "Angola",
-		"population": "12420476",
-		"lifeexpectancy": "42.7",
-		"purchasingpower": "4797.23"
-	},
-	{
-		"country": "Argentina",
-		"population": "40301927",
-		"lifeexpectancy": "75.3",
-		"purchasingpower": "12779.38"
-	},
-	{
-		"country": "Australia",
-		"population": "204341763",
-		"lifeexpectancy": "81.2",
-		"purchasingpower": "34435.37"
-	}
-];
-*/
+
 const x = d3.scaleLinear()
-    .domain([0, 35000])
+    .domain([0, 50000])
     .range([0, 800]);
 
 const y = d3.scaleLinear()
@@ -51,16 +12,15 @@ const y = d3.scaleLinear()
 class App extends Component {
 
     handleMouseOut(d, i) {
-        // Use D3 to select element, change color back to normal
+
         d3.select(this)
             .attr("fill", "#ffaaccaa");
-        // Select text by id and then remove
+
         d3.select("#t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
       }
 
-    handleMouseOver(d, i) {  // Add interactivity
-        // Use D3 to select element, change color and size
-        
+    handleMouseOver(d, i) {  
+                
         let parent = d3.select(this.parentNode);
 
         d3.select(this)
@@ -68,8 +28,8 @@ class App extends Component {
         
         parent.append("text")
             .attr("id", "t" + d.x + "-" + d.y + "-" + i)
-            .attr("x", d => x(d.purchasingpower) - 10)
-            .attr("y", d => y(d.lifeexpectancy))
+            .attr("x", d => x(d.purchasingPower) - 10)
+            .attr("y", d => y(d.lifeExpectancy))
             .text(d=>d.country);
 
         // Specify where to put label of text
@@ -88,10 +48,9 @@ class App extends Component {
 
     componentDidMount(){
 
-        fetch("https://gist.githubusercontent.com/josejbocanegra/000e838b77c6ec8e5d5792229c1cdbd0/raw/83cd9161e28e308ef8c5363e217bad2b6166f21a/countries.json")
-        .then(res => {
-            return res.json();
-        }).then(data => {
+        fetch("https://gist.githubusercontent.com/josejbocanegra/4c553e3b5f1aae1f05ea67068f058087/raw/9f1ec3f2b48cf59ed3c3c4b01d15d1a23b25f57c/countriesall.json")
+        .then(res => res.json())
+        .then(data => {
             this.renderData(data);
         });   
     }
@@ -103,14 +62,14 @@ class App extends Component {
         let main = svg.append("g")
             .attr("transform", "translate(50,50)");
 
-        let g = svg.selectAll("g").data(data);
+        let g = main.selectAll("g").data(data);
 
         var elemEnter = g.enter()
             .append("g");
 
         elemEnter.append("circle")
-            .attr("cx", d => x(d.purchasingpower))
-            .attr("cy", d => y(d.lifeexpectancy))
+            .attr("cx", d => x(d.purchasingPower))
+            .attr("cy", d => y(d.lifeExpectancy))
             .attr("r", 25)
             .on("mouseover", this.handleMouseOver)
             .on("mouseout", this.handleMouseOut)
