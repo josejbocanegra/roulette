5dd6004020372c3adf6ab99b3c53db6419f62bbc
Jose Bocanegra <josejbocanegra@gmail.com>
2020-02-24 17:04:38 -0500

    Adding react project

--- express/chat202010-mongodb/routes/chat.js
+++ express/chat202010-mongodb/routes/chat.js
@@ -2,10 +2,23 @@ var express = require('express');
 var router = express.Router();
 var socketApi = require("../socketApi");
 
-/* GET users listing. */
+var Mongolib = require("../db/Mongolib");
+
 router.post('/', function (req, res, next) {
-  socketApi.sendNotification(req.body);
-  res.send(req.body);
+  Mongolib.getDatabase(db => {
+    Mongolib.insertDocuments(db, data => {
+      socketApi.sendNotification();
+      res.send(req.body);
+    }, req.body)
+  });
+});
+
+router.get('/', function (req, res, next) {
+  Mongolib.getDatabase(db => {
+    Mongolib.findDocuments(db, docs => {
+      res.send(docs);
+    })
+  });
 });
 
 module.exports = router;
--- express/chat202010-mongodb/socketApi.js
+++ express/chat202010-mongodb/socketApi.js
@@ -2,26 +2,33 @@ let socket_io = require("socket.io");
 var io = socket_io();
 var socketApi = {};
 
+const Mongolib = require("./db/Mongolib");
+
 socketApi.io = io;
 
-let messages = [{
-    id: 1,
-    text: "Welcome to chat room",
-    author: "Chat admin"
-}];
-
-
 io.on('connection', function (socket) {
-    io.sockets.emit('messages', messages);
+
+    Mongolib.getDatabase(db => {
+        Mongolib.findDocuments(db, docs => {
+            io.sockets.emit('messages', docs);
+        });
+    })
 
     socket.on("new-message", data => {
-        socketApi.sendNotification(data)
+        Mongolib.getDatabase(db => {
+            Mongolib.insertDocuments(db, () => {
+                socketApi.sendNotification()
+            }, data);
+        })
     })
 });
 
-socketApi.sendNotification = data => {
-    messages.push(data);
-    io.sockets.emit('messages', messages);
+socketApi.sendNotification = () => {
+    Mongolib.getDatabase(db => {
+        Mongolib.findDocuments(db, docs => {
+            io.sockets.emit('messages', docs);
+        });
+    })
 }
 
 module.exports = socketApi;
