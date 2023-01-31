66ee4b6c28eb9b31c5c758795dfa0c17aa7a35a2
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2023-01-31 11:37:29 -0500

    update students

---
+++ Roulette/script.js
@@ -0,0 +1,332 @@
+var data = [
+  {
+    label: "Aguilera Figueroa Daniel Esteban",
+    question: "Aguilera Figueroa Daniel Esteban",
+    value: 1,
+  },
+  { label: "Cerino Pulido Javier", question: "Cerino Pulido Javier", value: 1 },
+  {
+    label: "Mendez Melo Julian Andres",
+    question: "Mendez Melo Julian Andres",
+    value: 1,
+  },
+  { label: "Rojas Becerra Luccas", question: "Rojas Becerra Luccas", value: 1 },
+  {
+    label: "Alegria Zuñiga Juan Sebastian",
+    question: "Alegria Zuñiga Juan Sebastian",
+    value: 1,
+  },
+  { label: "Falla Moreno Camilo", question: "Falla Moreno Camilo", value: 1 },
+  {
+    label: "Molina Mahecha Andres Felipe",
+    question: "Molina Mahecha Andres Felipe",
+    value: 1,
+  },
+  {
+    label: "Sanchez Ocampo Cristian Armando",
+    question: "Sanchez Ocampo Cristian Armando",
+    value: 1,
+  },
+  {
+    label: "Arroyo Castellanos Jose Cristobal",
+    question: "Arroyo Castellanos Jose Cristobal",
+    value: 1,
+  },
+  {
+    label: "Gamez Abril Kevin Steven",
+    question: "Gamez Abril Kevin Steven",
+    value: 1,
+  },
+  {
+    label: "Osorio Cardenas Daniel",
+    question: "Osorio Cardenas Daniel",
+    value: 1,
+  },
+  {
+    label: "Santos Ayala Juan Martin",
+    question: "Santos Ayala Juan Martin",
+    value: 1,
+  },
+  {
+    label: "Bernal Caceres Daniel Andres",
+    question: "Bernal Caceres Daniel Andres",
+    value: 1,
+  },
+  {
+    label: "Gomez Barrera Daniel Fernando",
+    question: "Gomez Barrera Daniel Fernando",
+    value: 1,
+  },
+  {
+    label: "Pardo Gutierrez Sergio",
+    question: "Pardo Gutierrez Sergio",
+    value: 1,
+  },
+  {
+    label: "Serna Contreras Armando Andres",
+    question: "Serna Contreras Armando Andres",
+    value: 1,
+  },
+  {
+    label: "Bonet de Vivero Juan Camilo",
+    question: "Bonet de Vivero Juan Camilo",
+    value: 1,
+  },
+  {
+    label: "Gonzalez Ruales Esteban",
+    question: "Gonzalez Ruales Esteban",
+    value: 1,
+  },
+  {
+    label: "Quiroga Alfaro Nathalia Alexandra",
+    question: "Quiroga Alfaro Nathalia Alexandra",
+    value: 1,
+  },
+  {
+    label: "Serrano Rodriguez Javier",
+    question: "Serrano Rodriguez Javier",
+    value: 1,
+  },
+  {
+    label: "Calixto Nuñez Juan Diego",
+    question: "Calixto Nuñez Juan Diego",
+    value: 1,
+  },
+  {
+    label: "Guatibonza Briceño Pablo Alejandro",
+    question: "Guatibonza Briceño Pablo Alejandro",
+    value: 1,
+  },
+  {
+    label: "Reales Paba Daniel Alfredo",
+    question: "Reales Paba Daniel Alfredo",
+    value: 1,
+  },
+  {
+    label: "Soto Parada Erich Giusseppe",
+    question: "Soto Parada Erich Giusseppe",
+    value: 1,
+  },
+  {
+    label: "Carrascal Cruz Maria Isabel",
+    question: "Carrascal Cruz Maria Isabel",
+    value: 1,
+  },
+  {
+    label: "Hidalgo Garcia Juan Pablo",
+    question: "Hidalgo Garcia Juan Pablo",
+    value: 1,
+  },
+  {
+    label: "Rivera Hernandez Brian Manuel",
+    question: "Rivera Hernandez Brian Manuel",
+    value: 1,
+  },
+  {
+    label: "Vargas Torres Maria Alejandra",
+    question: "Vargas Torres Maria Alejandra",
+    value: 1,
+  },
+  {
+    label: "Castaño Lozano Juan Felipe",
+    question: "Castaño Lozano Juan Felipe",
+    value: 1,
+  },
+  {
+    label: "Lugo Sanchez Juan Diego",
+    question: "Lugo Sanchez Juan Diego",
+    value: 1,
+  },
+  {
+    label: "Rodriguez Ospino Juan Esteban",
+    question: "Rodriguez Ospino Juan Esteban",
+    value: 1,
+  },
+  {
+    label: "Yara Ramirez Leandro Esteban",
+    question: "Yara Ramirez Leandro Esteban",
+    value: 1,
+  },
+  { label: "Castro Iregui Maria", question: "Castro Iregui Maria", value: 1 },
+  {
+    label: "Martinez Prieto Laura Vanessa",
+    question: "Martinez Prieto Laura Vanessa",
+    value: 1,
+  },
+  {
+    label: "Rodriguez Puentes Nicolas Alfonso",
+    question: "Rodriguez Puentes Nicolas Alfonso",
+    value: 1,
+  },
+  {
+    label: "Zuliani Escalante Marco",
+    question: "Zuliani Escalante Marco",
+    value: 1,
+  },
+];
+
+var padding = { top: 20, right: 40, bottom: 0, left: 0 },
+  w = 500 - padding.left - padding.right,
+  h = 500 - padding.top - padding.bottom,
+  r = Math.min(w, h) / 2,
+  rotation = 0,
+  oldrotation = 0,
+  picked = 100000,
+  oldpick = [],
+  color = d3.scale.category20(); //category20c()
+
+var svg = d3
+  .select("#chart")
+  .append("svg")
+  .data([data])
+  .attr("width", w + padding.left + padding.right)
+  .attr("height", h + padding.top + padding.bottom);
+var container = svg
+  .append("g")
+  .attr("class", "chartholder")
+  .attr(
+    "transform",
+    "translate(" + (w / 2 + padding.left) + "," + (h / 2 + padding.top) + ")"
+  );
+var vis = container.append("g");
+
+var pie = d3.layout
+  .pie()
+  .sort(null)
+  .value(function (d) {
+    return 1;
+  });
+// declare an arc generator function
+var arc = d3.svg.arc().outerRadius(r);
+// select paths, use arc generator to draw
+var arcs = vis
+  .selectAll("g.slice")
+  .data(pie)
+  .enter()
+  .append("g")
+  .attr("class", "slice");
+
+arcs
+  .append("path")
+  .attr("fill", function (d, i) {
+    return color(i);
+  })
+  .attr("d", function (d) {
+    return arc(d);
+  });
+// add the text
+arcs
+  .append("text")
+  .attr("transform", function (d) {
+    d.innerRadius = 0;
+    d.outerRadius = r;
+    d.angle = (d.startAngle + d.endAngle) / 2;
+    return (
+      "rotate(" +
+      ((d.angle * 180) / Math.PI - 90) +
+      ")translate(" +
+      (d.outerRadius - 10) +
+      ")"
+    );
+  })
+  .attr("text-anchor", "end")
+  .text(function (d, i) {
+    return data[i].label;
+  });
+container.on("click", spin);
+
+function spin(d) {
+  container.on("click", null);
+  //all slices have been seen, all done
+  console.log("OldPick: " + oldpick.length, "Data length: " + data.length);
+  if (oldpick.length == data.length) {
+    console.log("done");
+    container.on("click", null);
+    return;
+  }
+  var ps = 360 / data.length,
+    pieslice = Math.round(1440 / data.length),
+    rng = Math.floor(Math.random() * 1440 + 360);
+
+  rotation = Math.round(rng / ps) * ps;
+
+  picked = Math.round(data.length - (rotation % 360) / ps);
+  picked = picked >= data.length ? picked % data.length : picked;
+  if (oldpick.indexOf(picked) !== -1) {
+    d3.select(this).call(spin);
+    return;
+  } else {
+    oldpick.push(picked);
+  }
+  rotation += 90 - Math.round(ps / 2);
+  vis
+    .transition()
+    .duration(3000)
+    .attrTween("transform", rotTween)
+    .each("end", function () {
+      //mark question as seen
+      d3.select(".slice:nth-child(" + (picked + 1) + ") path").attr(
+        "fill",
+        "#111"
+      );
+      //populate question
+      d3.select("#question h1").text(data[picked].question);
+      oldrotation = rotation;
+
+      container.on("click", spin);
+    });
+}
+//make arrow
+svg
+  .append("g")
+  .attr(
+    "transform",
+    "translate(" +
+      (w + padding.left + padding.right) +
+      "," +
+      (h / 2 + padding.top) +
+      ")"
+  )
+  .append("path")
+  .attr("d", "M-" + r * 0.15 + ",0L0," + r * 0.05 + "L0,-" + r * 0.05 + "Z")
+  .style({ fill: "black" });
+//draw spin circle
+container
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 60)
+  .style({ fill: "white", cursor: "pointer" });
+//spin text
+container
+  .append("text")
+  .attr("x", 0)
+  .attr("y", 15)
+  .attr("text-anchor", "middle")
+  .text("SPIN")
+  .style({ "font-weight": "bold", "font-size": "30px" });
+
+function rotTween(to) {
+  var i = d3.interpolate(oldrotation % 360, rotation);
+  return function (t) {
+    return "rotate(" + i(t) + ")";
+  };
+}
+
+function getRandomNumbers() {
+  var array = new Uint16Array(1000);
+  var scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
+  if (
+    window.hasOwnProperty("crypto") &&
+    typeof window.crypto.getRandomValues === "function"
+  ) {
+    window.crypto.getRandomValues(array);
+    console.log("works");
+  } else {
+    //no support for crypto, get crappy random numbers
+    for (var i = 0; i < 1000; i++) {
+      array[i] = Math.floor(Math.random() * 100000) + 1;
+    }
+  }
+  return array;
+}
