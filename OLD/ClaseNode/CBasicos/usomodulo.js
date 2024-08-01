5f66dfa50b839da98279ae1bfb71b0bf500b5c94
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2024-08-01 11:49:19 -0500

    Fix structure

--- OLD/ClaseNode/CBasicos/usomodulo.js
+++
@@ -1,3 +0,0 @@
-let s = require("./modulo");
-
-console.log(s.sumar(25,30));
--- clase1/currying/sample1.js
+++
@@ -1,4 +0,0 @@
-//const mcd = (a, b) => (a === b ? a : mcd(Math.min(a, b), Math.abs(a - b)));
-
-const mcd = (a) => (b) => a === b ? a : mcd(Math.min(a, b), Math.abs(a - b));
-console.log("MCD", mcd(10)(5)()());
